e3cb771290d6bb0a18b4cfbbb50a4b14ba7f4634
neptune55 <oskar.kabaliuk@protonmail.com>
2023-10-28 21:20:50 +0200

    lalala

---
+++ Domain/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+
+    public Post(int id, string title, string body)
+    {
+        Id = id;
+        Title = title;
+        Body = body;
+    }
+}
---
+++ Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public User(int id, string username, string password)
+    {
+        Id = id;
+        Username = username;
+        Password = password;
+    }
+}
